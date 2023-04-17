0c2c96b5347c0ca342be822d59c78ddc8ab3cc50
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2023-04-17 14:50:51 +0530

    Inital commits

--- src/Components/Home/Filterbox/Filterbox.jsx
+++ src/Components/Home/Filterbox/Filterbox.jsx
@@ -6,86 +6,91 @@ import "./Filterbox.css";
 const Filterbox = () => {
   return (
     <section id="filterbox">
-      <div className="box">
-        <h2>Book a Car</h2>
-        <form>
-          <div className="filterbox_row">
-            <div className="form_label">
-              <label htmlFor="cartype">
-                <AiFillCar size={20} className="filerbox_icons"/>
-                Select you car type<span>*</span>
-              </label>
-              <select name="cartype" id="">
-                <option value="" disabled selected hidden>Select Your Car Type</option>
-                <option value="Audi">Audi</option>
-                <option value="BMW">BMW</option>
-                <option value="MG">MG</option>
-                <option value="Tata">Tata</option>
-                <option value="Suzuki">Suzuki</option>
-              </select>
-            </div>
-
-            {/* option-2 */}
-
-            <div className="form_label">
-              <label htmlFor="pickup">
-                <HiLocationMarker size={20} className="filerbox_icons"/>
-                Pick Up<span>*</span>
-              </label>
-              <select name="pickup" id="">
-                <option value="" disabled selected hidden>Select Pick Up Location</option>
-                <option value="Bhopal">Bhopal</option>
-                <option value="Mumbai">Mumbai</option>
-                <option value="Pune">Pune</option>
-                <option value="New Delhi">New Delhi</option>
-                <option value="Indore">Indore</option>
-              </select>
-            </div>
-
-            {/* Option-3 */}
-
-            <div className="form_label">
-              <label htmlFor="dropof">
-                <HiLocationMarker size={20} className="filerbox_icons"/>
-                Drop Off<span>*</span>
-              </label>
-              <select name="dropof" id="">
-                <option value="" disabled selected hidden>Select Drop of Location</option>
-                <option value="New Delhi">New Delhi</option>
-                <option value="Bhopal">Bhopal</option>
-                <option value="Pune">Pune</option>
-                <option value="Indore">Indore</option>
-                <option value="Mumbai">Mumbai</option>
-              </select>
-            </div>
+      <div className="inside_box">
+        <h2>Book a car</h2>
+        <div className="filterbox_row">
+          <div className="filterbox_column">
+            <label htmlFor="cartype">
+              <AiFillCar size={20} className="filerbox_icons"/>
+              Select Your Car Type<span> *</span>
+            </label>
+            <select name="cartype" id="">
+              <option value="" disabled selected hidden>
+                Select Your Car Type
+              </option>
+              <option value="Audi">Audi</option>
+              <option value="BMW">BMW</option>
+              <option value="MG">MG</option>
+              <option value="Tata">Tata</option>
+              <option value="Suzuki">Suzuki</option>
+            </select>
           </div>
 
-          {/* row-2 */}
+          {/* col 2 */}
 
-          <div className="filterbox_row">
-            <div className="form_label">
-              <label htmlFor="cardate">
-                <BsFillCalendarWeekFill size={20} className="filerbox_icons"/>
-                Pick-up<span>*</span>
-              </label>
-              <input type="date" name="cardate" />
-            </div>
+          <div className="filterbox_column">
+            <label htmlFor="pickup">
+              <HiLocationMarker size={20} className="filerbox_icons" />
+              Pick Up<span>*</span>
+            </label>
+            <select name="pickup" id="">
+              <option value="" disabled selected hidden>
+                Select Pick Up Location
+              </option>
+              <option value="Bhopal">Bhopal</option>
+              <option value="Mumbai">Mumbai</option>
+              <option value="Pune">Pune</option>
+              <option value="New Delhi">New Delhi</option>
+              <option value="Indore">Indore</option>
+            </select>
+          </div>
 
-            {/* option-2 */}
+          {/* col 3 */}
 
-            <div className="form_label">
-              <label htmlFor="cardate">
-                <BsFillCalendarWeekFill size={20} className="filerbox_icons"/>
-                Drop-off<span>*</span>
-              </label>
-              <input type="date" name="cardate" />
-            </div>
+          <div className="filterbox_column">
+            <label htmlFor="dropof">
+              <HiLocationMarker size={20} className="filerbox_icons" />
+              Drop Off<span>*</span>
+            </label>
+            <select name="dropof" id="">
+              <option value="" disabled selected hidden>
+                Select Drop of Location
+              </option>
+              <option value="New Delhi">New Delhi</option>
+              <option value="Bhopal">Bhopal</option>
+              <option value="Pune">Pune</option>
+              <option value="Indore">Indore</option>
+              <option value="Mumbai">Mumbai</option>
+            </select>
+          </div>
+        </div>
+        <div className="filterbox_row">
+          {/* col 4 */}
 
-            {/* Option-3 */}
+          <div className="filterbox_column">
+            <label htmlFor="cardate">
+              <BsFillCalendarWeekFill size={20} className="filerbox_icons" />
+              Pick-up<span>*</span>
+            </label>
+            <input type="date" name="cardate" />
+          </div>
 
+          {/* col 5 */}
+
+          <div className="filterbox_column">
+            <label htmlFor="cardate">
+              <BsFillCalendarWeekFill size={20} className="filerbox_icons" />
+              Pick-up<span>*</span>
+            </label>
+            <input type="date" name="cardate" />
+          </div>
+
+          {/* col 6 */}
+
+          <div className="filterbox_column">
             <input type="submit" value="Search" className="filterbox_button" />
           </div>
-        </form>
+        </div>
       </div>
     </section>
   );
