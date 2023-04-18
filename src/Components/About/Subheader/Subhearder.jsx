e784ffcd11ae0b140a06b999e0b29664b7551d29
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2023-04-18 16:39:19 +0530

    Initial Commits

--- src/Components/About/Subheader/Subhearder.jsx
+++ src/Components/About/Subheader/Subhearder.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./Subheader.css";
 import { Link } from "react-router-dom";
+import subheaderimg from "../../../Assets/subhero.png";
 
 const Subhearder = () => {
   return (
     <section id="subheader">
-      <div className="background_overlay"></div>
-        <h3>About</h3>
+      <img src={subheaderimg} alt="" />
+
+      <div className="left">
         <p>
+          <h3>About</h3>
           <Link to="/">Home</Link> / About
         </p>
+      </div>
     </section>
   );
 };
